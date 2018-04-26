4e32dbff928c823a7c66919d0ce61dfab7b6e5a3
nageshkmr1 <subrato.mozumdar@cbre.com>
2018-04-26 00:09:24 -0500

    try good

--- ios/UdemyFastlane/AppDelegate.m
+++ ios/UdemyFastlane/AppDelegate.m
@@ -1,23 +1,4 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-#import "AppDelegate.h"
-
-#import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-#import <BuddyBuildSDK/Bud
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-  [BuddyBuildSDK setup];
-  
-  NSURL *jsCodeLocation;dddddddddddd
+/nagesh try
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
